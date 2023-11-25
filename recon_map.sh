df055ef144daff8c6d97b087b320511e66e390be
G0TH3R <112994775+G0TH3R@users.noreply.github.com>
2023-11-24 21:36:13 -0500

    Update recon_map.sh

--- recon_map.sh
+++ recon_map.sh
@@ -11,12 +11,12 @@
 #     file for note-taking during the reconnaissance process.
 #
 # usage:
-#           sudo recon_map <folder Name> <ip address>
+#           sudo recon_map <folder Name> <ip address> -c
 #
 
 # Check if arguments are provided
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <VM name> <IP Address>"
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <VM name> <IP Address> [-c]"
     exit 1
 fi
 
@@ -32,10 +32,18 @@ target=$1
 ip_vm=$2
 
 
+create_additional_files=false
+if [ "$3" == "-c" ]; then
+    create_additional_files=true
+fi
+
+
 # Create Directory and Folder
 current_dir=$(pwd)
 folder_name="$target"
 path="$current_dir/$folder_name"
+
+
 # Banner 
 banner="
    ▄████████    ▄████████  ▄████████  ▄██████▄  ███▄▄▄▄   
@@ -50,6 +58,7 @@ banner="
    ▀█    █▀    ${YELLOW}Usage: ./recon_map <Target Name> <IP Address>${RESET}                                             
 "
 echo -e "\n${GREEN}$banner${RESET}"
+
 # Check if the directory already exists
 if [ -d "$path" ]; then
     echo -e "\n${RED}Directory already exists: $path${RESET}"
@@ -68,19 +77,10 @@ fi
 
 
 
-# Change the current directory
-cd "$path"
 
-# Check if the current directory has changed successfully
-if [ "$current_dir" != "$path" ]; then
-    echo -e "${GREEN}Current directory changed to: $path${RESET}"
-else
-    echo -e "${RED}Failed to change current directory to: $path${RESET}"
-    exit 1
-fi
 
 # Create the ScratchPad
-file_path="scratchPad_$target_$ip_vm.txt"
+file_path="$path/Notes-$target.md"
 
 # Check if the file exists
 if [ -f "$file_path" ]; then
@@ -95,6 +95,53 @@ else
         exit 1
     fi
 fi
+
+
+# Create additional files and folders if -c flag is present
+if [ "$create_additional_files" = true ]; then
+    # Check if creds.txt exists
+    if [ -f "creds.txt" ]; then
+        echo -e "${YELLOW}creds.txt already exists and will not be overwritten.${RESET}"
+    else
+        touch "creds.txt"
+        echo -e "${GREEN}creds.txt created.${RESET}"
+    fi
+
+    # Check if notes.md exists
+    if [ -f "notes.md" ]; then
+        echo -e "${YELLOW}notes.md already exists and will not be overwritten.${RESET}"
+    else
+        touch "notes.md"
+        echo -e "${GREEN}notes.md created.${RESET}"
+    fi
+
+    # Create files1 and files2 directories if they don't exist
+    for dir in files exploits loot; do
+        if [ -d "$dir" ]; then
+            echo -e "${YELLOW}$dir directory already exists.${RESET}"
+        else
+            echo -e "${GREEN}Attempting to create $dir directory at $dir${RESET}"
+            mkdir "$dir"
+            if [ $? -eq 0 ]; then
+                echo -e "${GREEN}Successfully created $dir directory.${RESET}"
+            else
+                echo -e "${RED}Failed to create $dir directory. Check permissions and path.${RESET}"
+            fi
+        fi
+    done
+fi
+
+# Change the current directory
+cd "$path"
+
+# Check if the current directory has changed successfully
+if [ "$current_dir" != "$path" ]; then
+    echo -e "${GREEN}Current directory changed to: $path${RESET}"
+else
+    echo -e "${RED}Failed to change current directory to: $path${RESET}"
+    exit 1
+fi
+
 
 # Update the Target IP Address on the top panel
 echo -e "${GREEN}\n\t[+] Updated Target: ${BLUE}$target${RESET} ${GREEN}IP: ${BLUE}$ip_vm ${GREEN}"
@@ -123,3 +170,5 @@ echo "These are the ports: $port_number"
 echo -e "${YELLOW}\n[+] Processing NMAP ${RESET}"
 nmap -Pn -sV -sC -p "$port_number" "$ip_vm" -oN "${target}_${ip_vm}-NMAP"
 echo -e "${GREEN}\n[>]Target: ${RED}$target${RESET} ${GREEN}IP:${RESET} ${RED}$ip_vm ${GREEN}Ports: ${YELLOW}$port_number${RESET}"
+
+sudo chown -R crimson:crimson $path 
